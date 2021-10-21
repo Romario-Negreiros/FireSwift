da2000e5dbacb1769464f919cf7c9cbf5adb40ee
Romario Negreiros <nromario482@gmail.com>
2021-10-21 01:53:27 -0300

    added verification to avoidd the user of addind itself as a friend + added try/catch blocks for error handling

--- src/utils/Friend.ts
+++ src/utils/Friend.ts
@@ -1,46 +1,56 @@
-import { User } from "../global/types";
+import { User } from '../global/types';
+
+import { toast } from 'react-toastify';
 
 import { firestoredb } from '../lib';
-import { useAppDispatch } from "../app/hooks";
-import { updateUser } from "../features/user/userSlice";
+import { updateUser } from '../features/user/userSlice';
 
-const dispatch = useAppDispatch();
+import { AppDispatch } from '../app/store';
 
 class Friend {
+  static remove = async (id: string, user: User, dispatch: AppDispatch) => {
+    try {
+      const userRef = firestoredb.doc(firestoredb.db, 'users', user.id);
+      const newFriendsList = user.friends.filter(fid => fid !== id);
 
-  static remove = async (id: string, user: User) => {
-    const userRef = firestoredb.doc(firestoredb.db, 'users', user.id)
+      await firestoredb.updateDoc(userRef, {
+        friends: [...newFriendsList],
+      });
 
-    const newFriendsList = user.friends.filter(fid => fid !== id);
+      const userSnap = await firestoredb.getDoc(userRef);
 
-    await firestoredb.updateDoc(userRef, {
-      friends: [...newFriendsList]
-    });
-
-    const userSnap = await firestoredb.getDoc(userRef);
-    if(userSnap.exists()) {
-      const uid = userSnap.id;
-      const userData = userSnap.data() as Omit<User, 'id'>;
-      dispatch(updateUser({ id: uid, ...userData }))
+      if (userSnap.exists()) {
+        const uid = userSnap.id;
+        const userData = userSnap.data() as Omit<User, 'id'>;
+        dispatch(updateUser({ id: uid, ...userData }));
+        toast("User removed from your friend's list");
+      } else toast.error("Looks like the user doesn't exist!");
+    } catch (err) {
+      if (err instanceof Error) toast.error(err.message);
     }
   };
 
-  static add = async (id: string, user: User) => {
-    const userRef = firestoredb.doc(firestoredb.db, 'users', user.id)
+  static add = async (id: string, user: User, dispatch: AppDispatch) => {
+    if (user.id !== id) {
+      try {
+        const userRef = firestoredb.doc(firestoredb.db, 'users', user.id);
 
-    await firestoredb.updateDoc(userRef, {
-      friends: [ id, ...user.friends]
-    });
-    
-    const userSnap = await firestoredb.getDoc(userRef);
-    if(userSnap.exists()) {
-      const id = userSnap.id;
-      const user = userSnap.data() as Omit<User, 'id'>
-      dispatch(updateUser({ id, ...user }))
-    }
-    
+        await firestoredb.updateDoc(userRef, {
+          friends: [id, ...user.friends],
+        });
+
+        const userSnap = await firestoredb.getDoc(userRef);
+        if (userSnap.exists()) {
+          const id = userSnap.id;
+          const user = userSnap.data() as Omit<User, 'id'>;
+          dispatch(updateUser({ id, ...user }));
+          toast("User added in your friend's list");
+        } else toast.error("Looks like the user doesn't exist!");
+      } catch (err) {
+        if (err instanceof Error) toast.error(err.message);
+      }
+    } else toast.error('You cannot add yourself as friend!')
   };
-
-};
+}
 
 export default Friend;
