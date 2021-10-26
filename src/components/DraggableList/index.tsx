efdd7af0efc4c4ce048c407812698ae192f35943
Romario Negreiros <nromario482@gmail.com>
2021-10-26 01:07:36 -0300

    implemented drag and drop between lists

--- src/components/DraggableList/index.tsx
+++ src/components/DraggableList/index.tsx
@@ -7,7 +7,7 @@ import { User } from '../../global/types';
 
 interface Data {
   name: string;
-  image?: string;
+  image: string;
 }
 
 interface Props {
@@ -20,19 +20,24 @@ const DraggableList: React.FC<Props> = ({ title, currentUser, data }) => {
   const [options, setOptions] = React.useState(data);
   const [userList, setUserList] = React.useState(currentUser.languages);
 
+  const setNewOrder = (options: Data[], userList: Data[]) => {
+    setOptions(options);
+    setUserList(userList);
+  }
+
   const onDragEnd = (result: DropResult) => {
     const { source, destination } = result;
     if (!destination) return;
-console.log(source, setOptions, setUserList)
-    // const leftColumn = [...options];
-    // const rightColumn = [...userList];
-    // if (source.droppableId === 'usersList') {
-    //   const [splicedItem] = rightColumn.splice(source.index, 1);
-    //   // leftColumn.splice(destination.index, 0, splicedItem as Data);
-    // } else if (source.droppableId === 'options') {
-    //   const [splicedItem] = leftColumn.splice(source.index, 1);
-    //   // rightColumn.
-    // }
+    const leftColumn = [...options];
+    const rightColumn = [...userList];
+    if (source.droppableId === 'userList') {
+      const [splicedItem] = rightColumn.splice(source.index, 1);
+      leftColumn.splice(destination.index, 0, splicedItem);
+    } else if (source.droppableId === 'options') {
+      const [splicedItem] = leftColumn.splice(source.index, 1);
+      rightColumn.splice(destination.index, 0, splicedItem);
+    }
+    setNewOrder(leftColumn, rightColumn);
   };
 
   const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
@@ -71,8 +76,8 @@ console.log(source, setOptions, setUserList)
           {provided => (
             <ul className="userList" {...provided.droppableProps} ref={provided.innerRef}>
               {provided.placeholder}
-              {userList.map((lang, i) => (
-                <Draggable key={`${lang}-${i}`} draggableId={`${lang}-${i}`} index={i}>
+              {userList.map((item, i) => (
+                <Draggable key={`${item.name}-${i}`} draggableId={`${item.name}-${i}`} index={i}>
                   {(provided, snapshot) => (
                     <li
                       ref={provided.innerRef}
@@ -80,7 +85,8 @@ console.log(source, setOptions, setUserList)
                       {...provided.dragHandleProps}
                       style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
                     >
-                      {lang}
+                      <span>{item.name}</span>
+                      <img src={item.image} alt={item.name} />
                     </li>
                   )}
                 </Draggable>
