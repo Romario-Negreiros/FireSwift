d5617692701915de60ddac8cacff4cb9a4e8ecd1
Romario Negreiros <nromario482@gmail.com>
2021-10-28 00:41:40 -0300

    created function to extract the items in the user list of the draggable components

---
+++ src/utils/GetUserListData.ts
@@ -0,0 +1,16 @@
+const GetUserListData = (lists: string[]) => {
+  const data: any = {};
+  lists.forEach(item => {
+    const ul = document.querySelector(`.userList-${item}`) as HTMLUListElement;
+    const children = Array.from(ul.children);
+    data[item] = children.map(child => {        
+      return {
+        name: child.firstElementChild?.innerHTML,
+        image: child.lastElementChild?.getAttribute('src'),
+      };
+    });
+  });
+  return data;
+};
+
+export default GetUserListData;
