b113dedccdf3cd75e66220b6277bb67e7d74f08f
Romario Negreiros <nromario482@gmail.com>
2021-10-29 23:13:12 -0300

    created list of hobbies to use as options

---
+++ src/app/hobbies.ts
@@ -0,0 +1,28 @@
+const hobbies: { name: string }[] = [
+  {
+    name: 'Sports',
+  },
+  {
+    name: 'Music',
+  },
+  {
+    name: 'Cook',
+  },
+  {
+    name: 'Work out',
+  },
+  {
+    name: 'Radical sports',
+  },
+  {
+    name: 'Art',
+  },
+  {
+    name: 'Videogames',
+  },
+  {
+    name: 'Going out',
+  },
+];
+
+export default hobbies;
