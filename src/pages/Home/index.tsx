a445e784d78c122a3841d2842e96b62439b30cd6
Romario Negreiros <nromario482@gmail.com>
2021-11-07 23:38:31 -0300

    rendering social options component + adjusted grid styled component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { FriendsList, Feed } from '../../components';
+import { FriendsList, Feed, SocialOptions } from '../../components';
 
 import { Grid } from './styles';
 
 const Home: React.FC = () => {
   return (
     <Grid>
+      <SocialOptions />
       <FriendsList />
       <Feed />
     </Grid>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,10 +2,13 @@ import styled from 'styled-components';
 
 export const Grid = styled.main`
   display: grid;
+  grid-template-rows: 120px 180px auto;
   grid-template-columns: 1fr;
-  grid-template-rows: 180px auto;
   gap: 2rem;
   padding: 5px;
-  width: 100vw;
+  width: 100%;
   min-height: 100vh;
+  @media screen and (min-width: 700px) {
+    grid-template-rows: 60px 180px auto;
+  }
 `;
