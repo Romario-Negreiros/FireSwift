36b2eff11351d2687ca5c8951992c8fff7f84493
Romario Negreiros <nromario482@gmail.com>
2021-11-29 22:19:37 -0300

    created function to change the creation view

--- src/pages/Create/index.tsx
+++ src/pages/Create/index.tsx
@@ -1,31 +1,56 @@
 import React from 'react';
 
+import { useAppSelector } from '../../app/hooks';
+
 import { Container, Options, View, DefaultView } from './styles';
-import { CreateGroup, CreatePost, CreateStory } from '../../components';
+import { CreateGroup, CreatePost, CreateStory, Exception } from '../../components';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGlassCheers, faPaw, faTable } from '@fortawesome/free-solid-svg-icons';
+import { CenteredContainer } from '../../global/styles';
 
 const Create: React.FC = () => {
-  const [currentComponent, setCurrentComponent] = React.useState('newgroup');
+  const [currentComponent, setCurrentComponent] = React.useState('');
+  const user = useAppSelector(state => state.user.user);
 
+  const changeComponent = () => {
+    if (user) {
+      switch (currentComponent) {
+        case 'New group':
+          return <CreateGroup />;
+        case 'New post':
+          return <CreatePost id={user.id} />;
+        case 'New story':
+          return <CreateStory />;
+        default:
+          return <DefaultView />;
+      }
+    } 
+  };
+  if(!user) {
+    return (
+      <CenteredContainer>
+        <Exception message={"You need to be logged in to create something!"}/>
+      </CenteredContainer>
+    )
+  }
   return (
     <Container>
       <h1>Create</h1>
       <Options>
-        <section onClick={() => setCurrentComponent('newgroup')}>
+        <section onClick={() => setCurrentComponent('New group')}>
           <p>New group</p>
           <div>
             <FontAwesomeIcon size="2x" icon={faPaw} color="purple" />
           </div>
         </section>
-        <section onClick={() => setCurrentComponent('newpost')}>
+        <section onClick={() => setCurrentComponent('New post')}>
           <p>New post</p>
           <div>
             <FontAwesomeIcon size="2x" icon={faTable} color="purple" />
           </div>
         </section>
-        <section onClick={() => setCurrentComponent('newstory')}>
+        <section onClick={() => setCurrentComponent('New story')}>
           <p>New story</p>
           <div>
             <FontAwesomeIcon size="2x" icon={faGlassCheers} color="purple" />
@@ -34,18 +59,7 @@ const Create: React.FC = () => {
       </Options>
       <View>
         <h2>{currentComponent}</h2>
-        {() => {
-          switch (currentComponent) {
-            case 'newgroup':
-              return <CreateGroup />;
-            case 'newpost':
-              return <CreatePost />;
-            case 'newstory':
-              return <CreateStory />;
-            default:
-              return <DefaultView />;
-          }
-        }}
+        {changeComponent()}
       </View>
     </Container>
   );
