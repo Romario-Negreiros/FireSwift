76e5f95a828680719d06da17496609e68212d5bf
Romario Negreiros <nromario482@gmail.com>
2021-12-13 15:00:47 -0300

    bug fixing

---
+++ src/utils/getUserListData.ts
@@ -0,0 +1,15 @@
+const getUserListData = (lists: string[]) => {
+  const data: any = {};
+  lists.forEach(item => {
+    const ul = document.querySelector(`.userList-${item}`) as HTMLUListElement;
+    const children = Array.from(ul.children);
+    data[item] = children.map(child => {        
+      return {
+        name: child.firstElementChild?.innerHTML,
+      };
+    });
+  });
+  return data;
+};
+
+export default getUserListData;
