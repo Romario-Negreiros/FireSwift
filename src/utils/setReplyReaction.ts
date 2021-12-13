89f22c09b16fbef4bd78c9e0fa4339d46d3ada86
Romario Negreiros <nromario482@gmail.com>
2021-12-13 11:14:22 -0300

    created function to set reactions in replies

---
+++ src/utils/setReplyReaction.ts
@@ -0,0 +1,45 @@
+import { firestoredb } from '../lib';
+
+import { toast } from 'react-toastify';
+import { Post } from '../global/types';
+
+const setReplyReaction = async (
+  userID: string,
+  commentID: string,
+  replyID: string,
+  post: Post,
+  posts: Post[],
+  setPosts: (posts: Post[]) => void,
+  newReaction: string
+) => {
+  try {
+    const postsCopy = [...posts];
+    const postIndex = postsCopy.findIndex(postCopy => postCopy.id === post.id);
+    const commentIndex = postsCopy[postIndex].comments.findIndex(
+      comment => comment.id === commentID
+    );
+    const replyIndex = postsCopy[postIndex].comments[commentIndex].replies.findIndex(
+      reply => reply.id === replyID
+    );
+    const reactionIndex = postsCopy[postIndex].comments[commentIndex].replies[replyIndex].reactions.findIndex(
+      reaction => reaction.id === userID
+    );
+    if (reactionIndex === -1) {
+      postsCopy[postIndex].comments[commentIndex].replies[replyIndex].reactions.push({
+        id: userID,
+        reaction: newReaction,
+      });
+    } else {
+      postsCopy[postIndex].comments[commentIndex].replies[replyIndex].reactions[reactionIndex].reaction = newReaction;
+    }
+    const postRef = firestoredb.doc(firestoredb.db, 'media/posts/users', post.id);
+    await firestoredb.updateDoc(postRef, {
+      comments: postsCopy[postIndex].comments,
+    });
+    setPosts(postsCopy);
+  } catch (err) {
+    if (err instanceof Error) toast.error('Something unnexpected happened!');
+  }
+};
+
+export default setReplyReaction;
