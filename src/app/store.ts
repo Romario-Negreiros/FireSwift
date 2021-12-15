0230c2a7e528f783244569d9de8b8eedb5a87cab
Romario Negreiros <nromario482@gmail.com>
2021-12-14 23:25:12 -0300

    added new reducer to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/user/userSlice';
+import usersReducer from '../features/users/usersSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    users: usersReducer,
   }
 });
 
