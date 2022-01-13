b79fbc865f0475d17e0dcad18c70acba82afcc7e
Romario Negreiros <nromario482@gmail.com>
2022-01-13 12:15:48 -0300

    added notifications to setPostReaction and setReplyReaction functions

--- src/utils/setters/setPostReaction.ts
+++ src/utils/setters/setPostReaction.ts
@@ -1,23 +1,38 @@
 import { firestoredb } from '../../lib';
 import handleError from '../general/handleError';
+import getFormattedDate from '../getters/getFormattedDate';
+import { v4 as uuidv4 } from 'uuid';
 
-import { Post } from '../../global/types';
+import { Post, User, Notification } from '../../global/types';
 
 const setPostReaction = async (
-  id: string,
+  user: User,
   post: Post,
   posts: Post[],
   setPosts: (posts: Post[]) => void,
   newReaction: string,
-  pathSegment: string,
+  pathSegment: string
 ) => {
   try {
+    const newNotification: Notification = {
+      id: uuidv4(),
+      sentBy: {
+        id: user.id,
+        name: user.name,
+        picture: user.picture,
+      },
+      wasViewed: false,
+      message: `${user.name} reacted to your post!`,
+      sentAt: getFormattedDate(),
+    };
     const postsCopy = [...posts];
     const postIndex = postsCopy.findIndex(postCopy => postCopy.id === post.id);
-    const reactionIndex = postsCopy[postIndex].reactions.findIndex(reaction => reaction.id === id);
+    const reactionIndex = postsCopy[postIndex].reactions.findIndex(
+      reaction => reaction.id === user.id
+    );
     if (reactionIndex === -1) {
       postsCopy[postIndex].reactions.push({
-        id,
+        id: user.id,
         reaction: newReaction,
       });
     } else {
@@ -27,6 +42,15 @@ const setPostReaction = async (
     await firestoredb.updateDoc(postRef, {
       reactions: postsCopy[postIndex].reactions,
     });
+    const postAuthorRef = firestoredb.doc(firestoredb.db, 'users', post.author.id);
+    const postAutSnap = await firestoredb.getDoc(postAuthorRef);
+    if (postAutSnap.exists()) {
+      const authorData = postAutSnap.data() as Omit<User, 'id'>;
+      authorData.notifications.unshift(newNotification);
+      await firestoredb.updateDoc(postAuthorRef, {
+        notifications: authorData.notifications,
+      });
+    }
     setPosts(postsCopy);
   } catch (err) {
     handleError(err, 'setting reaction in a post.');
--- src/utils/setters/setReplyReaction.ts
+++ src/utils/setters/setReplyReaction.ts
@@ -1,19 +1,32 @@
 import { firestoredb } from '../../lib';
 import handleError from '../general/handleError';
+import getFormattedDate from '../getters/getFormattedDate';
+import { v4 as uuidv4 } from 'uuid';
 
-import { Post } from '../../global/types';
+import { Post, User, Notification } from '../../global/types';
 
 const setReplyReaction = async (
-  userID: string,
+  user: User,
   commentID: string,
   replyID: string,
   post: Post,
   posts: Post[],
   setPosts: (posts: Post[]) => void,
   newReaction: string,
-  pathSegment: string,
+  pathSegment: string
 ) => {
   try {
+    const newNotification: Notification = {
+      id: uuidv4(),
+      sentBy: {
+        id: user.id,
+        name: user.name,
+        picture: user.picture,
+      },
+      wasViewed: false,
+      message: `${user.name} reacted with a ${newReaction} in your reply in a comment!`,
+      sentAt: getFormattedDate(),
+    };
     const postsCopy = [...posts];
     const postIndex = postsCopy.findIndex(postCopy => postCopy.id === post.id);
     const commentIndex = postsCopy[postIndex].comments.findIndex(
@@ -24,16 +37,29 @@ const setReplyReaction = async (
     );
     const reactionIndex = postsCopy[postIndex].comments[commentIndex].replies[
       replyIndex
-    ].reactions.findIndex(reaction => reaction.id === userID);
+    ].reactions.findIndex(reaction => reaction.id === user.id);
     if (reactionIndex === -1) {
       postsCopy[postIndex].comments[commentIndex].replies[replyIndex].reactions.push({
-        id: userID,
+        id: user.id,
         reaction: newReaction,
       });
     } else {
       postsCopy[postIndex].comments[commentIndex].replies[replyIndex].reactions[
         reactionIndex
       ].reaction = newReaction;
+    }
+    const replyAuthorRef = firestoredb.doc(
+      firestoredb.db,
+      'users',
+      postsCopy[postIndex].comments[commentIndex].replies[replyIndex].author.id
+    );
+    const repAutSnap = await firestoredb.getDoc(replyAuthorRef);
+    if (repAutSnap.exists()) {
+      const authorData = repAutSnap.data() as Omit<User, 'id'>;
+      authorData.notifications.unshift(newNotification);
+      await firestoredb.updateDoc(replyAuthorRef, {
+        notifications: authorData.notifications,
+      });
     }
     const postRef = firestoredb.doc(firestoredb.db, `media/posts/${pathSegment}`, post.id);
     await firestoredb.updateDoc(postRef, {
